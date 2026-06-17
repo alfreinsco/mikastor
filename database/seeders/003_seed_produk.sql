0120a7d727e8d6912ab3864dcc39825d6815a613
marthinsalakory <marthinsalakory11@gmail.com>
2026-06-17 16:24:01 +0900

    Update product seed data with corrected prices and stock quantities

--- database/seeders/003_seed_produk.sql
+++ database/seeders/003_seed_produk.sql
@@ -1,8 +1,9 @@
 INSERT INTO produk (nama_produk, harga, stok)
 VALUES
-    ('Minyak Kayu Putih 60ml', 15000, 50),
-    ('Minyak Kayu Putih 120ml', 28000, 30),
-    ('Minyak Kayu Putih 210ml', 50000, 20)
+    ('Minyak Kayu Putih 60ml', 13500, 1088),
+    ('Minyak Kayu Putih 120ml', 27000, 594),
+    ('Minyak Kayu Putih 210ml', 47250, 339),
+    ('Minyak Kayu Putih 1Kg', 225000, 3133)
 ON DUPLICATE KEY UPDATE
     harga = VALUES(harga),
     stok = VALUES(stok);
